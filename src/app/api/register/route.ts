81889faa617ea25882fd2bc0c1ba0ce4eb4f4cbd
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-07-02 00:53:48 +0500

    type changed

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { email, username, password } = body;
 
-  const hashedPassword = await bcrypt.hash(password, 12);
+  const hashedPassword: string | null = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
     data: {
