467ac0e762476495cf10c2b5e76c024fc901b245
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-07-03 10:14:22 +0500

    type changed

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,16 +2,6 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
-import { User } from ".prisma/client"; // Adjust import based on Prisma setup
-
-interface CreateUserInput {
-  username: string;
-  email: string;
-  hashedPassword: string; // Include hashedPassword in interface
-  phoneNumber: string | null;
-  userType: string;
-  balance: number;
-}
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -20,18 +10,12 @@ export async function POST(request: Request) {
   const hashedPassword: string = await bcrypt.hash(password, 12);
 
   try {
-    const userData: CreateUserInput = {
-      username,
-      email,
-      hashedPassword,
-      phoneNumber: null,
-      userType: "Customer",
-      balance: 0.0,
-    };
-
-    // Since Prisma may not directly accept hashedPassword, we handle it separately
     const user = await prisma.user.create({
-      data: userData as User, // Type assertion to User type from Prisma
+      data: {
+        username,
+        email,
+        hashedPassword,
+      },
     });
 
     return NextResponse.json(user);
